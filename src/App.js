d5b7d0b5650291af6ffd68fef79af4b12cf55416
JorgeTafur <jorgetafurp@gmail.com>
2024-07-26 21:43:32 -0500

    avance

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import { useState } from 'react';
 import './App.css';
+import Axios from "axios"
 
 function App() {
 
-  const [nombre, setNombre] = useState("");
-  const [edad, setEdad] = useState(0);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [language, setLanguage] = useState("");
 
-  const mostrarDatos = () => {
-    alert(nombre)
+  const add = () => {
+    Axios.post("http://localhost:8000/api/students", {
+      name: name, 
+      email: email,
+      phone: phone,
+      language: language
+    }).then(() => {
+      alert("Empleado registrado")
+    });
   }
 
   return (
     <div className="App">
       <div className="datos">
-        <label>Nombre: <input onChange={(event)=>{setNombre(event.target.value)}} type="text"/></label><br/><br/>
-        <label>Edad: <input onChange={(event)=>{setEdad(event.target.value)}} type="number"/></label><br/><br/>
-        <button onClick={mostrarDatos}>Registrar</button>
+        <label>Nombre: <input onChange={(event)=>{setName(event.target.value)}} type="text"/></label><br/><br/>
+        <label>Email: <input onChange={(event)=>{setEmail(event.target.value)}} type="text"/></label><br/><br/>
+        <label>Phone: <input onChange={(event)=>{setPhone(event.target.value)}} type="text"/></label><br/><br/>
+        <label>Language: <input onChange={(event)=>{setLanguage(event.target.value)}} type="text"/></label><br/><br/>
+        <button onClick={add}>Registrar</button>
       </div>
     </div>
   );
