101295f60b83ad5b0f5cadd747b70ea73815417c
JorgeTafur <jorgetafurp@gmail.com>
2024-07-31 00:01:25 -0500

    test

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const cors = require("cors");
+
+App.use(cors());
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
